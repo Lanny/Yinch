2a722ecae4e0f84b65654e5fa07f088a562844ec
RyanJenkins <Ryan.Jenkins@wellsfargo.com>
2016-01-01 13:30:47 -0800

    Cleared out gh-pages.

--- src/clj/yinch/repl.clj
+++
@@ -1,9 +0,0 @@
-(ns yinch.repl)
-
-(require 'cljs.repl)
-(require 'cljs.repl.browser)
-
-(defn -main []
-  (cljs.repl/repl (cljs.repl.browser/repl-env)
-                    :watch "src/cljs"
-                    :output-dir ".out"))
--- src/cljs/yinch/repl.cljs
+++
@@ -1,9 +0,0 @@
-(ns yinch.repl
-  (:require
-    [clojure.browser.repl :as repl]
-    [cemerick.url :as url]))
-
-(enable-console-print!)
-
-(defn ^:export connect []
-  (repl/connect "http://localhost:9000/repl"))
